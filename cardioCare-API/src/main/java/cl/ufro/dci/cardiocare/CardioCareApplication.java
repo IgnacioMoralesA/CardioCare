cded390df378d1ab68df08c3434bf30799007dfe
IGNACIO ANDRES ANTONIO MORALES ALMEYDA <i.morales04@ufromail.cl>
2025-10-27 10:11:57 -0300

    init api and ui

---
+++ cardioCare-API/src/main/java/cl/ufro/dci/cardiocare/CardioCareApplication.java
@@ -0,0 +1,13 @@
+package cl.ufro.dci.cardiocare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardioCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardioCareApplication.class, args);
+    }
+
+}
---
+++ cardioCare-API/src/test/java/cl/ufro/dci/cardiocare/CardioCareApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ufro.dci.cardiocare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardioCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
